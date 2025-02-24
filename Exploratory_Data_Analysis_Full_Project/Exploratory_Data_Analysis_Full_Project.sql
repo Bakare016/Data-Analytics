79fa71d57364189074f5fc7bfa9aad19fae20ab8
Kehinde <bakareolasunkanmi016@gmail.com>
2025-02-24 19:33:14 +0100

    Add more codes

--- Exploratory_Data_Analysis_Full_Project/Exploratory_Data_Analysis_Full_Project.sql
+++ Exploratory_Data_Analysis_Full_Project/Exploratory_Data_Analysis_Full_Project.sql
@@ -37,7 +37,76 @@ order by 1 desc;
 select stage, sum(total_laid_off)
 from layoffs_staging2
 group by stage
-order by 1 desc;
+order by 2 desc;
+
+
+select company, avg(percentage_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+
+select substring(`date`,1,7) as `MONTH`, SUM(total_laid_off)
+from layoffs_staging2
+where substring(`date`,1,7) is not NULL
+group by `MONTH`
+order by 1 asc
+;
+
+
+with rolling_total as 
+(
+select substring(`date`,1,7) as `MONTH`, SUM(total_laid_off) as Total_Off
+from layoffs_staging2
+where substring(`date`,1,7) is not NULL
+group by `MONTH`
+order by 1 asc
+)
+select `MONTH`, total_off,
+sum(Total_Off) over(order by `MONTH`) as rolling_total
+from rolling_total;
+
+
+
+select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+order by 3 desc;
+
+with Company_Year (Company, years, total_laid_off) as
+(
+select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+), Company_Year_Rank as
+(select *, 
+dense_rank() over (partition by years order by total_laid_off desc) as Ranking
+from Company_Year
+where years is not null
+)
+select *
+from Company_Year_Rank
+;
+
+
+
+###################################################################### I love Jesus ########################################
+
+
+
+
+
+
+
+
+
+
+
 
 
 
